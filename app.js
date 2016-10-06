1cdc6fc1042dd7ddc97bf6fa50c0058240564df0
Laure <laure.milian@gmail.com>
2016-10-06 14:59:31 +0200

    html & css clean

--- app.js
+++ app.js
@@ -5,17 +5,17 @@
 	$(".empty").on("click", function() {
 		count = count + 1;
 		valeur_case = $(this).data("type");
-		console.log(valeur_case);
 
 		if (count % 2 === 1 && valeur_case === "clickOK") {
-			console.log(count);
 			$(this).removeClass("empty");
 			$(this).addClass("red");
+			$(this).data("type", "clickForbidden");			
 			$(this).prev().data("type", "clickOK");
 
 		} else if (valeur_case === "clickOK") {
 			$(this).removeClass("empty");
 			$(this).addClass("yellow");
+			$(this).data("type", "clickForbidden");
 			$(this).prev().data("type", "clickOK");
 
 		} else {
